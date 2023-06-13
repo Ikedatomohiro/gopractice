42bf114fde68fd759f7def2ebe04b46cb536133d
Tomohiro Ikeda <to.tomo.tomohiro@gmail.com>
2023-06-13 22:30:37 +0900

    add lesson

---
+++ learning/section7/49goroutine/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	// "time"
+)
+
+func goroutine(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 0; i < 5; i++ {
+		// time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
+func normal(s string) {
+	for i := 0; i < 5; i++ {
+		// time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	defer wg.Wait()
+	go goroutine("world", &wg)
+	normal("hell0")
+	// time.Sleep(1000 * time.Millisecond)
+}
