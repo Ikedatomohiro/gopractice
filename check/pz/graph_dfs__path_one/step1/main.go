94f00540c93eebc5e16d54166f9e7f0939a8082c
Tomohiro Ikeda <to.tomo.tomohiro@gmail.com>
2023-06-27 23:50:24 +0900

    modify graph

--- check/pz/graph_dfs__path_one/step1/main.go
+++ check/pz/graph_dfs__path_one/step1/main.go
@@ -1,3 +1,4 @@
+// https://paiza.jp/works/mondai/graph_dfs_problems/graph_dfs__path_one_step1
 package main
 
 import (
@@ -5,4 +6,24 @@ import (
 )
 
 func main() {
+	var (
+		n, s, a, v, max int
+	)
+	max = 0
+	fmt.Scan(&n, &s)
+	g := make([][]int, n)
+
+	for i := 0; i < n; i++ {
+		fmt.Scan(&a)
+		for j := 0; j < a; j++ {
+			fmt.Scan(&v)
+			g[i] = append(g[i], v)
+		}
+	}
+	for _, val := range g[s-1] {
+		if max < val {
+			max = val
+		}
+	}
+	fmt.Println(max)
 }
